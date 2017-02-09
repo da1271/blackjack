628451dc64e9b26497f0b77d76a5abd298ac2136
da1271 <dwyleahn@gmail.com>
2017-02-08 16:29:33 -0800

    Updated JS. Finished 90%

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 console.log('connected')
 
 var suits = ['Spades', 'Hearts', 'Clubs', 'Diamonds']
-var vals = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King']
+var vals = ['ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king']
+
 
 var Card = function(suit, val) {
     this.suit = suit
@@ -30,52 +31,114 @@ function getCard(deck) {
 var userHand;
 var dealerHand;
 
-function dealCards() {
-    userHand = [getCard(deck), getCard(deck)];
-    dealerHand = [getCard(deck), getCard(deck)];
-    $('#hitButton').click(function() {
-        userHand.push(getCard(deck))
-    })
-    $('#stayButton').click(function() {
-        for (var i = 0; i < userHand.length; i++) {
-            if (userHand[i].val === 'Jack' || userHand[i].val === 'Queen' || userHand[i].val === 'King') {
-                userHand[i].val = 10;
-            } else if (dealerHand[i].val === 'Jack' || dealerHand[i].val === 'Queen' || dealerHand[i].val === 'King') {
-                dealerHand[i].val = 10;
-            } else if (userHand[i].val === 'Ace') {
-                userHand[i].val = 11;
-            } else if (dealerHand[i].val === 'Ace') {
-                dealerHand[i].val = 11;
-            }
+var userScore = 0;
+var dealerScore = 0;
 
-  function dealerHit() {
-      for (var i = 0; i < dealerHand.length; i++) {
-      //if (dealerHand[i] <= 17){
-        if (dealerHand[0] + dealerHand[1] <= 17){
-          dealerHand.push(getCard(deck));
+function getSumOfCards() {
+    userScore = 0;
+    dealerScore = 0;
+    for (var i = 0; i < userHand.length; i++) {
+        if (userHand[i].val === 'jack' || userHand[i].val === 'queen' || userHand[i].val === 'king') {
+            userHand[i].val = 10;
+        } else if (userHand[i].val === 'ace') {
+            userHand[i].val = 11;
         }
-  }
-        }
-        gameWinner();
-    })
+    }
+    for (var j = 0; j < dealerHand.length; j++) {
+        if (dealerHand[j].val === 'jack' || dealerHand[j].val === 'queen' || dealerHand[j].val === 'king') {
+            dealerHand[j].val = 10;
+        } else if (dealerHand[j].val === 'ace') {
+            dealerHand[j].val = 11;
+        }}
 
-}
-dealCards();
-
-function gameWinner() {
-    if (userHand === 21) {
-        console.log("congrats! you scored 21")
-    } else if (userHand > 21) {
-        console.log("sorry, you have gone over 21")
-    } else if (dealerHand > 21) {
-        console.log("The dealer is busted!")
-    } else if (userHand > dealerHand && userHand <= 21) {
-        console.log("you have beat the dealer!")
-    } else if (dealerHand > userHand && dealerHand <= 21) {
-        console.log("sorry the dealer beat you!")
-    } else {
-        console.log("It is a tie!")
+    for (var i = 0; i < userHand.length; i++) {
+        userScore = userScore + userHand[i].val
+    }
+    for (var j = 0; j < dealerHand.length; j++) {
+        dealerScore = dealerScore + dealerHand[j].val
     }
 }
 
-// Questions: how to make div appear on screen. if you need "+" for hands,
+function dealCards() {
+    userHand = [getCard(deck), getCard(deck)];
+    dealerHand = [getCard(deck), getCard(deck)];
+    $('#dealerCard1').html('<img src="playing_cards/PNG-cards-1.3/' + dealerHand[0].val + '_of_' + dealerHand[0].suit.toLowerCase() + '.png" alt="' + this.val1 + ' of ' + this.suit1 + '" />')
+    $('#dealerCard2').html('<img src="playing_cards/PNG-cards-1.3/' + dealerHand[1].val + '_of_' + dealerHand[1].suit.toLowerCase() + '.png" alt="' + this.val1 + ' of ' + this.suit1 + '" />')
+    $('#userCard1').html('<img src="playing_cards/PNG-cards-1.3/' + userHand[0].val + '_of_' + userHand[0].suit.toLowerCase() + '.png" alt="' + this.val1 + ' of ' + this.suit1 + '" />')
+    $('#userCard2').html('<img src="playing_cards/PNG-cards-1.3/' + userHand[1].val + '_of_' + userHand[1].suit.toLowerCase() + '.png" alt="' + this.val1 + ' of ' + this.suit1 + '" />')
+
+        getSumOfCards();
+
+
+        var newcardcount = 3
+        $('#hitButton').click(function() {
+            userHand.push(getCard(deck))
+            $('#userCard' + newcardcount).html('<img src="playing_cards/PNG-cards-1.3/' + userHand[userHand.length - 1].val + '_of_' + userHand[userHand.length - 1].suit.toLowerCase() + '.png" alt="' + this.val1 + ' of ' + this.suit1 + '" />')
+            newcardcount++;
+            getSumOfCards();
+
+
+        })
+        $('#stayButton').click(function() {
+            var newcardcount2 = 3
+            while (dealerScore <= 17) {
+                    dealerHand.push(getCard(deck));
+                    $('#dealerCard' + newcardcount2).html('<img src="playing_cards/PNG-cards-1.3/' + dealerHand[dealerHand.length - 1].val + '_of_' + dealerHand[userHand.length - 1].suit.toLowerCase() + '.png" alt="' + this.val1 + ' of ' + this.suit1 + '" />')
+                    newcardcount2++;
+                    getSumOfCards();
+
+                }gameWinner();
+
+        })
+
+    }
+    dealCards();
+    gameWinner();
+    $('#reset-game').click(function() {
+      location.reload();
+    })
+    // maybe dealCards before determining winner? / div/ betting system.
+
+
+
+    function gameWinner() {
+        if (userScore === 21) {
+            $('#winNer').text("congrats! you scored 21")
+        } else if (userScore > 21) {
+            $('#winNer').text("sorry, you have gone over 21")
+        } else if (dealerScore > 21) {
+            $('#winNer').text("The dealer is busted!")
+        } else if (userScore > dealerScore && userScore <= 21) {
+            $('#winNer').text("you have beat the dealer!")
+        } else if (dealerScore > userScore && dealerScore <= 21) {
+            $('#winNer').text("sorry the dealer beat you!")
+        } else {
+            $('#winNer').text("It is a tie!")
+        }
+    }
+
+    // Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("stayButton");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
